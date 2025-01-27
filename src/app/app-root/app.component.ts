a6f875cc2697b273abd4bddbe59bf41af1960373
Gal Ben David <gal.benda3@gmail.com>
2025-01-27 13:07:37 +0200

    Add app-header component

--- src/app/app-root/app.component.ts
+++ src/app/app-root/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RecipeIndexComponent } from '../pages/recipe-index/recipe-index.component';
+import { AppHeaderComponent } from '../cmps/app-header/app-header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RecipeIndexComponent],
+  imports: [RouterOutlet, RecipeIndexComponent, AppHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
