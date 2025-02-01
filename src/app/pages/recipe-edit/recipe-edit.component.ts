3bc38a58c40c68759b0ea4990fce4d79c4a333c1
Gal Ben David <gal.benda3@gmail.com>
2025-02-01 13:51:43 +0200

    Fetch recipe data in recipe-edit component using subscription

--- src/app/pages/recipe-edit/recipe-edit.component.ts
+++ src/app/pages/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, DestroyRef, inject, Input } from '@angular/core';
 import { RecipeModel } from '../../models/recipe.model';
+import { filter, map, Observable, switchMap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'recipe-edit',
@@ -8,6 +11,18 @@ import { RecipeModel } from '../../models/recipe.model';
   styleUrl: './recipe-edit.component.scss'
 })
 export class RecipeEditComponent {
+  private route = inject(ActivatedRoute)
+  private destroyRef = inject(DestroyRef)
+  recipe!: RecipeModel
 
+  ngOnInit(): void {
+    this.route.data.pipe(
+      map(data => data['recipe']),
+      filter(recipe => !!recipe),
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe(recipe => {
+      this.recipe = recipe
+    })
+  }
 
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { RecipeModel } from '../models/recipe.model';
 import { recipes } from '../data/data';
+import { UserService } from './user.service';
+import { UserModel } from '../models/user.model';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +13,31 @@ export class RecipeService {
 
   private _recipes$ = new BehaviorSubject<RecipeModel[]>([])
   public recipes$ = this._recipes$.asObservable()
+  private loggedInUser!: UserModel
 
-  constructor() {
+
+  constructor(private userService: UserService) {
     this._recipes$.next(recipes)
+
+    this.userService.loggedInUser$.pipe(
+      takeUntilDestroyed() // Automatically unsubscribes when component is destroyed
+    ).subscribe(user => {
+      this.loggedInUser = user
+    })
+  }
+
+  public getEmptyRecipe(): Partial<RecipeModel> {
+    return {
+      txt: '',
+      imgUrl: '',
+      by: {
+        _id: this.loggedInUser._id,
+        fullname: this.loggedInUser.fullname,
+        imgUrl: this.loggedInUser.imgUrl,
+      },
+      comments: [],
+      likedBy: [],
+      tags: [],
+    }
   }
 }
