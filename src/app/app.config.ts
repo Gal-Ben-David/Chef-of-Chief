5baef3ff577af3abf131d2caceb101c176bce38a
Gal Ben David <gal.benda3@gmail.com>
2025-02-09 13:02:46 +0200

    Use Hash for routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routers'
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routers';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    importProvidersFrom(RouterModule.forRoot(routes, { useHash: true }))
+  ]
 };
+// export const appConfig: ApplicationConfig = {
+//   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes,{ useHash: true })]
+// };
